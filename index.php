d1993b496f65ed91392919756a446bf92cfee17e
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-09 16:42:56 +0200

    add the header with h1, form with input type text and another one with type submit, and a list in index.php + add the folder Admin with addBooks.php in it

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
     <title>Document</title>
 </head>
 <body>
+
+    <header>
+        <h1>Get-A-Book</h1>
+        
+        <form action="#" method="POST">
+            <input type="text" id="">
+            <input type="submit" value="Submit">
+        </form>
+
+        <ul>
+            <a href="#"><li>Connexion</li></a>
+            <a href="#"><li>Inscription</li></a>
+        </ul>
+
+    <p>
+    <!-- Display last books in the database here -->
+    </p>
+    
+    </header>
     <?php
         include('./database/connection_database.php');
     ?>

40becb29cc3b4fdcbddd1909089f47132735e172
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-09 23:00:28 +0200

    add verification for see if an input is empty or not in addBook.php

--- database/addBooks.php
+++ database/addBooks.php
@@ -1,9 +1,18 @@
 <?php
-include('connection_database.php');
 
-$insertBook = $bdd->prepare('INSERT INTO addbook(nameBook,descBook,price) VALUES(:nameBook, :descBook, :price)');
-$insertBook->bindParam(':nameBook', $_POST['name']);
-$insertBook->bindParam(':descBook', $_POST['desc']);
-$insertBook->bindParam(':price', $_POST['price']);
-$insertBook->execute();
+
+
+if(!empty($_POST['name'])||!empty($_POST['desc'])||!empty($_POST['price'])){
+    include('connection_database.php');
+
+    $insertBook = $bdd->prepare('INSERT INTO addbook(nameBook,descBook,price) VALUES(:nameBook, :descBook, :price)');
+    $insertBook->bindParam(':nameBook', $_POST['name']);
+    $insertBook->bindParam(':descBook', $_POST['desc']);
+    $insertBook->bindParam(':price', $_POST['price']);
+    $insertBook->execute();
+    header('Location: ../Admin/addBooks.php');
+}
+else{
+    header('Location: ../Admin/addBooks.php');
+}
 ?>

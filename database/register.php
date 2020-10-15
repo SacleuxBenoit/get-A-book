f9189e2e2d7e68b377215cc33e117cfea48109d1
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-15 19:30:00 +0200

    add a verification for see if an email adress is already taken or not in database/register.php

--- database/register.php
+++ database/register.php
@@ -1,15 +1,23 @@
 <?php
-session_start();
 include('connection_database.php');
 
-if(empty($_POST['RegisterEmail']) || empty($_POST['RegisterPass'])){
+$verifyEmailDatabase = $bdd->prepare('SELECT email FROM user WHERE email = :email');
+$verifyEmailDatabase->bindParam(':email', $_POST['RegisterEmail']);
+$verifyEmailDatabase->execute();
+
+$verifyEmail = $verifyEmailDatabase->fetch();
+
+if($verifyEmail['email'] == $_POST['RegisterEmail']){
     header('Location: ../layouts/login_register.php');
-}else{
+}
+else if(empty($_POST['RegisterEmail']) || empty($_POST['RegisterPass'])){
+    header('Location: ../layouts/login_register.php');
+}
+else{
     $pass_hash = password_hash($_POST['RegisterPass'], PASSWORD_DEFAULT);
     $user_info = $bdd->prepare('INSERT INTO user(email,pass) VALUES(:email, :pass)');
     $user_info->bindParam(':email', $_POST['RegisterEmail']);
     $user_info->bindParam(':pass', $pass_hash);
     $user_info->execute();
 }
-
 ?>

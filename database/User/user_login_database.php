d101a40b7380411521da224e75a06b8906be623d
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-30 16:31:36 +0200

    replace email by username && create  id_user :arrow_right: database/User/user_login_database.php

--- database/User/user_login_database.php
+++ database/User/user_login_database.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
-include('connection_database.php');
+include('../../login_database.php');
+include('../connection_database.php');
 
-$user_login = $bdd->prepare('SELECT email,pass FROM user WHERE email = :email');
+$user_login = $bdd->prepare('SELECT id_user,username,pass FROM user WHERE email = :email');
 $user_login->bindParam(':email', $_POST['LoginEmail']);
 $user_login->execute();
 $donnees = $user_login->fetch();
 
 if($_POST['LoginEmail'] == $donnees['email'] && password_verify($_POST['LoginPass'],$donnees['pass'])){
+    $_SESSION['id_user'] = $donnees['id_user'];
     header('Location: ../../index.php');
 }else{
     header('Location: ../../layouts/login_register.php');

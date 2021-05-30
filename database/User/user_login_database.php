e498407b28014c17122820fd87d16182cba46d86
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-30 16:32:57 +0200

    :memo: hotfixe : replace email by username (forgot to save) :arrow_right: User/user_login_database.php

--- database/User/user_login_database.php
+++ database/User/user_login_database.php
@@ -3,12 +3,12 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
-$user_login = $bdd->prepare('SELECT id_user,username,pass FROM user WHERE email = :email');
-$user_login->bindParam(':email', $_POST['LoginEmail']);
+$user_login = $bdd->prepare('SELECT id_user,username,pass FROM user WHERE username = :username');
+$user_login->bindParam(':username', $_POST['Loginusername']);
 $user_login->execute();
 $donnees = $user_login->fetch();
 
-if($_POST['LoginEmail'] == $donnees['email'] && password_verify($_POST['LoginPass'],$donnees['pass'])){
+if($_POST['Loginusername'] == $donnees['username'] && password_verify($_POST['LoginPass'],$donnees['pass'])){
     $_SESSION['id_user'] = $donnees['id_user'];
     header('Location: ../../index.php');
 }else{

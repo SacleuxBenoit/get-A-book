f20e63af59a1666279224acc9b28f0c4da5623eb
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-01 15:03:50 +0200

    update header.php && add include for components/header.php :arrow_right: index.php

--- index.php
+++ index.php
@@ -12,37 +12,9 @@ include('./database/connection_database.php');
     <title>Document</title>
 </head>
 <body>
-
-    <header>
-        <h1>Get-A-Book</h1>
-        
-        <form action="#" method="POST">
-            <input type="text" id="InputSearchBar" placeholder="Search...">
-            <input type="submit" value="Submit">
-        </form>
-
-        <ul>
-            <a href="./layouts/login_register.php"><li>Login | Register</li></a>
-            <a href="#"><li>Orders</a></li>
-            <a href="./layouts/Admin/addBooks.php"><li>Add books</a></li>
-        </ul>
-
-        <ul class="navTop">
-            <li class="bd"><a href="#">BD</a></li>
-            <li class="devPerso"><a href="#">Développement personnel</a></li>
-            <li class="droitEconomie"><a href="#">Droit & économie</a></li>
-            <li class="histoire"><a href="#">Histoire</a></li>
-            <li class=jeunesse><a href="#">Jeunesse</a></li>
-            <li class="Litterature"><a href="#">Littérature</a></li>
-            <li class="policierSuspensThrillers"><a href="#">Policier, suspense, thrillers</a></li>
-            <li class="religion"><a href="#">Religion et spiritualité</a></li>
-            <li class="sfFantasy"><a href="#">SF, fantasy</a></li>
-            <li class="scolaire"><a href="#">Scolaire</a></li>
-            
-        </ul>
-
-    </header>
-
+    <?php
+        include('./components/header.php');
+    ?>
     <h2>lastest additions</h2>
         <?php
             $getLastBooks = $bdd->query('SELECT * FROM books ORDER BY id DESC LIMIT 5');

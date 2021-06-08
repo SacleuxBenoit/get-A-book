0195c31bc4458c88b642914b0ae55c5e0fa5ccaa
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-06-08 16:24:48 +0200

    add another css link for the component header

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include('./database/connection_database.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style_components/header.css">
     <link rel="stylesheet" href="css/style_index.css">
     <title>Document</title>
 </head>
--- layouts/categorie.php
+++ layouts/categorie.php
@@ -9,6 +9,7 @@ include('../database/connection_database.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style_components/header.css">
     <link rel="stylesheet" href="../css/style_categorie.css">
     <title>categorie</title>
 </head>
